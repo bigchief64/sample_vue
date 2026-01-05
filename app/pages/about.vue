12aa3ce570113d47f9999ec5e1d43b51a7c8234a
Jeremy Webb <bigchief526@gmail.com>
2026-01-05 09:58:00 -0600

    make email clickable and blue

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -3,6 +3,10 @@
         <PageTitle title="About" />
         <p>My name is Jeremy Webb</p>
         <p>I am a software developer with experience in Vue.js, Nuxt.js, and modern web technologies.</p>
-        <p>Please contact me at j.pwebb@yahoo.com</p>
+        <p>Please contact me at
+            <span class="text-blue-600">
+                <a href="mailto:j.pwebb@yahoo.com?subject=Inquiry">j.pwebb@yahoo.com</a>
+            </span>
+        </p>
     </section>
 </template>
